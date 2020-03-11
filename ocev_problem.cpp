702c6022bd5935965704e858497122df955ff07c
Ariel <ariel.agne95@gmail.com>
2020-03-11 16:57:19 -0300

    quase pronto

--- ocev_problem.cpp
+++ ocev_problem.cpp
@@ -10,11 +10,37 @@ public:
     Individual(string cod, int max_chromosomo){
 
         chromossomo = vector<int>(max_chromosomo);
+        if(cod == "bin"){
+            auto dist =  [](){
+                random_device g_rd;
+                mt19937 g_e(g_rd());
+                uniform_int_distribution<> g_dist(0, 1);
+
+                return g_dist(g_e);
+            };
+
+        }else if(cod == "int-perm"){
+            auto dist = [](int x, int y){
+                for(int i = 0; i < population.size(); i++){
+                    for(int j = 0; j < population[i].size(); j++){
+                        population[i][j] = j;
+                    }
+                }
 
 
+                for (int i = 0; i < population.size(); i++){
+                    for (int j = 0; j < population[i].size(); j++){
+                        int r = dist_int(0, max-1);
+
+                        int temp = population[i][j];
+                        population[i][j] = population[i][r];
+                        population[i][r] = temp;
+                    }
+                }
+            };
 
         for (size_t i = 0; i < max_chromosomo; i++) {
-            chromossomo[i] = distribution();
+            chromossomo[i] = binario();
         }
     }
 
@@ -39,7 +65,8 @@ int main(int argc, char const *argv[]) {
 	}
 
     for (size_t i = 0; i < individual; i++) {
-        cout << population[i] -> chromossomo << " ";
+        population[i] -> print_individual();
+        cout << endl;
     }
 
     return 0;
